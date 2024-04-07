1e3164dcaaa51bc6c0e03ce2fdcb618dd38361d6
castmygame <castmygameinc@gmail.com>
2024-04-07 00:39:48 -0400

    Removed unused imports and variables as well as console logs

--- src/components/dashboard/admin/widget/top-class-with-write-up.js
+++ src/components/dashboard/admin/widget/top-class-with-write-up.js
@@ -1,36 +1,36 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
-import React, { useEffect, useState } from "react";
-import { baseUrl } from "../../../../utils/jsonData";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import React from "react";
 
-export const WorseClassTable = ({ data = [],teacherData=[] }) => {
-
-
-
- 
-
-  // const getTeacherWithMostIncidents = (classItem) => {
-  //   const maxBlock = Math.max(classItem.blocks.block1, classItem.blocks.block2, classItem.blocks.block3, classItem.blocks.block4);
-  //   const teachersWithMaxBlock = teacherData.filter((teacher) => {
-  //     const negWriteUpData = data.filter((item) => item.infraction.infractionName !== "Positive Behavior Shout Out!" && item.teacherEmail === teacher.email);
-  //     const blockIncidents = negWriteUpData.filter((item) => item.classPeriod === `block${maxBlock}`).length;
-  //     return blockIncidents > 0;
-  //   });
-
-  //   const teacherWithMostIncidents = teachersWithMaxBlock.reduce((maxTeacher, currentTeacher) => {
-  //     const blockIncidents = data.filter((item) => item.teacherEmail === currentTeacher.email && item.classPeriod === `block${maxBlock}`).length;
-  //     return blockIncidents > maxTeacher.incidents ? { ...currentTeacher, incidents: blockIncidents } : maxTeacher;
-  //   }, { incidents: -1 });
-
-  //   return teacherWithMostIncidents.email;
-  // };
+export const WorseClassTable = ({ data = [], teacherData = [] }) => {
 
   // Move this declaration inside the component function
   const worseClassByIncident = teacherData.map((teacher) => {
-    const negWriteUpData = data.filter((item) => item.infractionName !== "Positive Behavior Shout Out!" && item.teacherEmail === teacher.email);
-    const block1 = negWriteUpData.filter((item) => item.classPeriod === "block1").length;
-    const block2 = negWriteUpData.filter((item) => item.classPeriod === "block2").length;
-    const block3 = negWriteUpData.filter((item) => item.classPeriod === "block3").length;
-    const block4 = negWriteUpData.filter((item) => item.classPeriod === "block4").length;
+    const negWriteUpData = data.filter(
+      (item) =>
+        item.infractionName !== "Positive Behavior Shout Out!" &&
+        item.teacherEmail === teacher.email
+    );
+    const block1 = negWriteUpData.filter(
+      (item) => item.classPeriod === "block1"
+    ).length;
+    const block2 = negWriteUpData.filter(
+      (item) => item.classPeriod === "block2"
+    ).length;
+    const block3 = negWriteUpData.filter(
+      (item) => item.classPeriod === "block3"
+    ).length;
+    const block4 = negWriteUpData.filter(
+      (item) => item.classPeriod === "block4"
+    ).length;
 
     return {
       teacherName: teacher.email,
@@ -40,69 +40,101 @@ export const WorseClassTable = ({ data = [],teacherData=[] }) => {
     };
   });
 
-  const worseBlk1 = worseClassByIncident.reduce((maxTeacher, currentTeacher) => {
-    return currentTeacher.blocks.block1 > maxTeacher.blocks.block1 ? currentTeacher : maxTeacher;
-  }, { teacherName: "", blocks: { block1: -1, block2: -1, block3: -1, block4: -1 } });
-  
+  const worseBlk1 = worseClassByIncident.reduce(
+    (maxTeacher, currentTeacher) => {
+      return currentTeacher.blocks.block1 > maxTeacher.blocks.block1
+        ? currentTeacher
+        : maxTeacher;
+    },
+    {
+      teacherName: "",
+      blocks: { block1: -1, block2: -1, block3: -1, block4: -1 },
+    }
+  );
 
-  const worseBlk2 = worseClassByIncident.reduce((maxTeacher, currentTeacher) => {
-    return currentTeacher.blocks.block2 > maxTeacher.blocks.block2 ? currentTeacher : maxTeacher;
-  }, { teacherName: "", blocks: { block: -1, block2: -1, block3: -1, block4: -1 } });
-  
-  const worseBlk3 = worseClassByIncident.reduce((maxTeacher, currentTeacher) => {
-    return currentTeacher.blocks.block3 > maxTeacher.blocks.block3 ? currentTeacher : maxTeacher;
-  }, { teacherName: "", blocks: { block: -1, block2: -1, block3: -1, block4: -1 } });
-  
-  const worseBlk4 = worseClassByIncident.reduce((maxTeacher, currentTeacher) => {
-    return currentTeacher.blocks.block4 > maxTeacher.blocks.block4 ? currentTeacher : maxTeacher;
-  }, { teacherName: "", blocks: { block: -1, block2: -1, block3: -1, block4: -1 } });
-  
+  const worseBlk2 = worseClassByIncident.reduce(
+    (maxTeacher, currentTeacher) => {
+      return currentTeacher.blocks.block2 > maxTeacher.blocks.block2
+        ? currentTeacher
+        : maxTeacher;
+    },
+    {
+      teacherName: "",
+      blocks: { block: -1, block2: -1, block3: -1, block4: -1 },
+    }
+  );
 
+  const worseBlk3 = worseClassByIncident.reduce(
+    (maxTeacher, currentTeacher) => {
+      return currentTeacher.blocks.block3 > maxTeacher.blocks.block3
+        ? currentTeacher
+        : maxTeacher;
+    },
+    {
+      teacherName: "",
+      blocks: { block: -1, block2: -1, block3: -1, block4: -1 },
+    }
+  );
 
+  const worseBlk4 = worseClassByIncident.reduce(
+    (maxTeacher, currentTeacher) => {
+      return currentTeacher.blocks.block4 > maxTeacher.blocks.block4
+        ? currentTeacher
+        : maxTeacher;
+    },
+    {
+      teacherName: "",
+      blocks: { block: -1, block2: -1, block3: -1, block4: -1 },
+    }
+  );
 
   return (
     <>
-    <Typography>
-      Classes With Highest Write Up By Period
-    </Typography>
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Period</TableCell>
-            <TableCell>Teacher</TableCell>
-            <TableCell>Number of Write Ups</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-    
+      <Typography>Classes With Highest Write Up By Period</Typography>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Period</TableCell>
+              <TableCell>Teacher</TableCell>
+              <TableCell>Number of Write Ups</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
             <TableRow>
               <TableCell>Block 1</TableCell>
-              <TableCell>{worseBlk1.teacherFirstName + " " + worseBlk1.teacherLastName}</TableCell>
+              <TableCell>
+                {worseBlk1.teacherFirstName + " " + worseBlk1.teacherLastName}
+              </TableCell>
               <TableCell>{worseBlk1.blocks.block1}</TableCell>
             </TableRow>
 
             <TableRow>
               <TableCell>Block 2</TableCell>
-              <TableCell>{worseBlk2.teacherFirstName + " " + worseBlk2.teacherLastName}</TableCell>
+              <TableCell>
+                {worseBlk2.teacherFirstName + " " + worseBlk2.teacherLastName}
+              </TableCell>
               <TableCell>{worseBlk2.blocks.block2}</TableCell>
             </TableRow>
 
             <TableRow>
               <TableCell>Block 3</TableCell>
-              <TableCell>{worseBlk3.teacherFirstName + " " + worseBlk3.teacherLastName}</TableCell>
+              <TableCell>
+                {worseBlk3.teacherFirstName + " " + worseBlk3.teacherLastName}
+              </TableCell>
               <TableCell>{worseBlk3.blocks.block3}</TableCell>
             </TableRow>
 
             <TableRow>
               <TableCell>Block 4</TableCell>
-              <TableCell>{worseBlk4.teacherFirstName + " " + worseBlk4.teacherLastName}</TableCell>
+              <TableCell>
+                {worseBlk4.teacherFirstName + " " + worseBlk4.teacherLastName}
+              </TableCell>
               <TableCell>{worseBlk4.blocks.block4}</TableCell>
             </TableRow>
-          
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 };
