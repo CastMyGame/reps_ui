c92a00757ac63c76200b47b1bb598493a380c632
David Anthony Perez <107569920+dperezskillstorm@users.noreply.github.com>
2024-09-03 21:05:58 -0500

    update landing headers

--- src/components/landing/header.jsx
+++ src/components/landing/header.jsx
@@ -1,38 +1,36 @@
-import React from "react";
-//
 export const Header = (props) => {
   return (
-    <header id="header">
+    <div>
+          <div style={{marginTop:20, padding:"20px 30px",background: "rgba(129, 127, 127, 0.884)"}}>
+      <h1>
+  {props.data ? props.data.title : "Loading"}
+  <span></span>
+</h1>
+<p>{props.data ? props.data.paragraph : "Loading"}</p>
+<a
+  href="#features"
+  className="btn btn-custom btn-lg page-scroll"
+>
+  Learn More
+</a>{" "}
+        </div>
+    <header style={{display:"flex",flexDirection:'column'}} id="header">
       <div className="intro">
         <div className="overlay">
           <div className="container">
-            <div className="row">
-              <div className="col-md-8 intro-text">
-                <div style={{ padding:"20px 30px",background: "rgba(129, 127, 127, 0.884)"}}>
-
-                <h1>
-                  {props.data ? props.data.title : "Loading"}
-                  <span></span>
-                </h1>
-                <p>{props.data ? props.data.paragraph : "Loading"}</p>
-                <a
-                  href="#features"
-                  className="btn btn-custom btn-lg page-scroll"
-                >
-                  Learn More
-                </a>{" "}
-
-                </div>
-
-                <div style={{}}>
-                  
-                  </div>
-              
+            <div  className="row">
+              <div  style={{height:"500px"}}className="col-md-8 intro-text">
+                
               </div>
             </div>
           </div>
         </div>
       </div>
+    
     </header>
+
+
+    </div>
   );
 };
+
